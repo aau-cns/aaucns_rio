3d0be27428317caec2487e6e610ade45f44eb4d7
Jan Michalczyk <jan.michalczyk@aau.at>
2025-03-12 15:04:26 +0100

    add missing license mentions and change few thresholds.

--- nodes/rio_node.cpp
+++ nodes/rio_node.cpp
@@ -1,3 +1,14 @@
+// Copyright (C) 2024 Jan Michalczyk, Control of Networked Systems, University
+// of Klagenfurt, Austria.
+//
+// All rights reserved.
+//
+// This software is licensed under the terms of the BSD-2-Clause-License with
+// no commercial use allowed, the full terms of which are made available
+// in the LICENSE file. No license in patents is granted.
+//
+// You can contact the author at <jan.michalczyk@aau.at>
+
 #include <ros/ros.h>
 
 #include "aaucns_rio/config.h"
